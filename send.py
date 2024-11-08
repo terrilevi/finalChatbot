ad385dd7d71cec84443770f3720599504ca5bf23
terrilevi <118628583+terrilevi@users.noreply.github.com>
2024-11-08 11:18:00 -0500

    Update send.py

--- send.py
+++ send.py
@@ -2,8 +2,8 @@ import twilio
 
 from twilio.rest import Client
 
-account_sid = 'ACe3f2f5a5a421a8e8d3ddccf451d36f68'  # Identifies your Twilio account
-auth_token = 'e831805998cd8e478a5e708a7958881f'     # Secret key for authentication
+account_sid = 'xxx'  # Identifies your Twilio account
+auth_token = 'xxx'     # Secret key for authentication
 
 # Initialize client
 client = Client(account_sid, auth_token)
